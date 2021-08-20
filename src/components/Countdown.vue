d93716c867291916dad379ea22ade242f9099099
Alexis La Torre <alexis-latorre@live.fr>
2021-08-20 15:14:13 +0200

    Implement the Countdown component

---
+++ src/components/Countdown.vue
@@ -0,0 +1,130 @@
+<template>
+  <div
+    id="countdown"
+    :style="`height: ${Math.ceil(timerRadius * 2.5)}px; width: ${Math.ceil(
+      timerRadius * 2.5
+    )}px`"
+  >
+    <div
+      id="countdown-number"
+      :style="`line-height: ${Math.ceil(
+        timerRadius * 2.5
+      )}px; font-size: ${Math.ceil(timerRadius * 0.8)}px`"
+    >
+      {{ Math.round(timer) }}
+    </div>
+    <svg
+      :style="`height: ${Math.ceil(timerRadius * 2.5)}px; width: ${Math.ceil(
+        timerRadius * 2.5
+      )}px`"
+    >
+      <circle
+        id="background-circle"
+        :style="`stroke-dasharray: ${rad}px;stroke-width: ${strokeThickness}px`"
+        :r="timerRadius"
+        :cx="Math.ceil(timerRadius * 1.25)"
+        :cy="Math.ceil(timerRadius * 1.25)"
+      ></circle>
+      <circle
+        id="countdown-circle"
+        :r="timerRadius"
+        :cx="Math.ceil(timerRadius * 1.25)"
+        :cy="Math.ceil(timerRadius * 1.25)"
+        :style="`stroke-dasharray: ${rad}px;stroke-width: ${strokeThickness}px;
+          stroke-dashoffset: ${rad - timer * (rad / timerLimit)}px;
+          stroke: ${
+            (timer / timerLimit) * 100 >= 30
+              ? 'green'
+              : (timer / timerLimit) * 100 >= 10
+              ? 'orange'
+              : 'red'
+          }`"
+      ></circle>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      timer: 0,
+      timerLimit: 10,
+      interval: null,
+      rad: 252,
+      timerRadius: 40,
+      strokeThickness: 10,
+    };
+  },
+  props: {
+    timeLimit: Number,
+    radius: Number,
+    thickness: Number,
+    startOn: Boolean,
+    callback: {
+      name: String,
+      args: Object,
+    },
+  },
+  methods: {
+    init() {
+      this.timer = this.$props.timeLimit ? this.$props.timeLimit : 10;
+      this.interval = setInterval(() => {
+        this.timer -= 0.01;
+        if (this.timer <= 0) {
+          this.timer = 0;
+          clearInterval(this.interval);
+
+          if (this.$props.callback && this.$props.callback.args)
+            this.$props.callback.func(this.$props.callback.args);
+        }
+      }, 10);
+    },
+  },
+  mounted() {
+    this.timerLimit = this.$props.timeLimit ? this.$props.timeLimit : 10;
+    this.timerRadius = this.$props.radius ? this.$props.radius : 40;
+    this.rad = Math.ceil(2 * Math.PI * this.timerRadius);
+    this.strokeThickness = this.$props.thickness
+      ? this.$props.thickness
+      : Math.round(this.timerRadius / 4);
+    if (this.$props.startOn) this.init();
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../css/variables.scss";
+
+#countdown {
+  position: relative;
+  margin: auto;
+  text-align: center;
+}
+
+#countdown-number {
+  color: $blue;
+  display: inline-block;
+  font-weight: bold;
+}
+
+svg {
+  position: absolute;
+  top: 0;
+  right: 0;
+  transform: rotateY(-180deg) rotateZ(-90deg);
+}
+
+#background-circle {
+  stroke-dashoffset: 0px;
+  stroke-linecap: round;
+  stroke: #eee;
+  fill: none;
+}
+
+#countdown-circle {
+  stroke-dashoffset: 0px;
+  stroke-linecap: round;
+  fill: none;
+}
+</style>
--- src/prods/tests/VariousTests.vue
+++ src/prods/tests/VariousTests.vue
@@ -1,158 +1,36 @@
 <!-- Don't mind this file, it's my sandbox -->
 <template>
-  <p>Your score: {{ score }}</p>
-  <table class="stats">
-    <tr>
-      <th>Question</th>
-      <th>Answer given</th>
-      <th>Correct answer</th>
-      <th>Question points</th>
-    </tr>
-    <tr
-      v-for="line of stats"
-      :key="line.question"
-      :class="`${line.answerGiven !== line.correctAnswer ? 'in' : ''}correct`"
-    >
-      <td v-html="line.question"></td>
-      <td v-html="line.answerGiven"></td>
-      <td v-html="line.correctAnswer"></td>
-      <td>{{ line.points }}</td>
-    </tr>
-    <tr>
-      <td colspan="2"></td>
-      <th>Total</th>
-      <th>{{ score }} / {{ maxScore }}</th>
-    </tr>
-  </table>
+  <Countdown
+    :timeLimit="10"
+    :radius="100"
+    :startOn="true"
+    :callback="callback"
+  />
 </template>
 
 <script>
+import Countdown from "@/components/Countdown";
+
 export default {
   data() {
     return {
-      score: 10,
-      maxScore: 15,
-      stats: [
-        {
-          question:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porta, nisl a scelerisque pulvinar, mi felis tincidunt orci, non ultrices quam ipsum ac libero.",
-          answerGiven: "Answer 1",
-          correctAnswer: "Answer 2",
-          points: 3,
+      callback: {
+        func: this.tadam,
+        name: "tadam",
+        args: {
+          name: "World",
+          text: "Hello, %s!",
         },
-        {
-          question: "Short question.",
-          answerGiven: "Answer 1",
-          correctAnswer: "Answer 1",
-          points: 3,
-        },
-        {
-          question:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porta, nisl a scelerisque pulvinar, mi felis tincidunt orci, non ultrices quam ipsum ac libero.",
-          answerGiven: "Answer 1",
-          correctAnswer: "Answer 1",
-          points: 3,
-        },
-        {
-          question: "Short question.",
-          answerGiven: "Answer 1",
-          correctAnswer: "Answer 2",
-          points: 3,
-        },
-        {
-          question:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porta, nisl a scelerisque pulvinar, mi felis tincidunt orci, non ultrices quam ipsum ac libero.",
-          answerGiven: "Answer 1",
-          correctAnswer: "Answer 1",
-          points: 3,
-        },
-      ],
+      },
     };
+  },
+  methods: {
+    tadam(args) {
+      console.log(args.text.replace("%s", args.name));
+    },
+  },
+  components: {
+    Countdown: Countdown,
   },
 };
 </script>
-
-<style lang="scss" scoped>
-@import "../../css/variables.scss";
-
-.stats {
-  margin-top: 1rem;
-  $radius: 0.5em;
-  $correct: green;
-  $incorrect: red;
-  border-collapse: separate;
-  border-spacing: 0;
-  border-radius: 1rem;
-
-  th {
-    font-size: 0.9rem;
-    padding: 0.8em;
-  }
-
-  td {
-    font-size: 0.8rem;
-    padding: 0.6em;
-  }
-
-  th,
-  td {
-    text-align: left;
-    vertical-align: middle;
-    border-left: solid 1px $purple;
-    border-top: solid 1px $purple;
-
-    &:last-child {
-      text-align: center;
-      border-right: solid 1px $purple;
-    }
-  }
-
-  .correct td {
-    color: $correct;
-    background: lighten($correct, 70%);
-  }
-
-  .incorrect td {
-    color: $incorrect;
-    background: lighten($incorrect, 40%);
-  }
-
-  tr:first-child {
-    *:first-child {
-      border-top-left-radius: $radius;
-    }
-    *:nth-last-child(1) {
-      border-top-right-radius: $radius;
-    }
-  }
-
-  tr:last-child {
-    *:first-child {
-      border: none;
-    }
-  }
-
-  tr:nth-last-child(1) {
-    * {
-      border-bottom: solid 1px $purple;
-      border-top: none;
-    }
-    *:nth-child(2) {
-      border-bottom-left-radius: $radius;
-    }
-    *:nth-last-child(1) {
-      border-bottom-right-radius: $radius;
-    }
-  }
-
-  tr:nth-last-child(2) {
-    * {
-      border-bottom: solid 1px $purple;
-    }
-
-    *:first-child {
-      border-bottom-left-radius: $radius;
-    }
-  }
-}
-</style>
